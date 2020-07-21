6dfd8678cafa1052f98d4ffcc15c5a3cb9564ebe
kudzanayi <kdzvairo@gmail.com>
2020-07-20 20:57:41 -0400

    no nav

--- src/Containers/PortfolioContainer.js
+++ src/Containers/PortfolioContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Portfolio from '../Components/Tables/Portfolio'
 import BuyStockForm from '../Components/Forms/BuyStock'
-import Navbar from '../Components/Navbar/Navbar'
+// import Navbar from '../Components/Navbar/Navbar'
 
 
 
@@ -11,7 +11,7 @@ const PortfolioContainer = (props) => {
 
         return (
             <>
-            <Navbar logOut={logOut} user={user}/>
+            // <Navbar logOut={logOut} user={user}/>
             <div className="p-container">
                <Portfolio user={user} transactions={transactions}/>
                <BuyStockForm handleOnSubmitBuyButton={props.handleOnSubmitBuyButton} user={props.user}/>
