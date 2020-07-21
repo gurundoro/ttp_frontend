74bbe8748d90fae296c77fcae52b506e7ba81337
kudzanayi <kdzvairo@gmail.com>
2020-07-20 21:00:14 -0400

    no nav

--- src/Components/Tables/Transactions.js
+++ src/Components/Tables/Transactions.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TransactionRow from './TransactionRow'
-import Navbar from '../Navbar/Navbar'
+// import Navbar from '../Navbar/Navbar'
 
 
 const Transactions = (props) => {
@@ -9,7 +9,7 @@ const Transactions = (props) => {
     
     return (
     <>
-       <Navbar logOut={logOut} user={user}/>
+    //    <Navbar logOut={logOut} user={user}/>
        <div className="container mb-3 mt-3">
        <h1 className="text-center display-1">Transactions</h1>
         <table className="table thead-dark table-bordered style={{width: '100%'}}">
